7a8b3674b5543f2fbd8177d3ebf8302682e36937
Mai Tran <maitran215@mac.com>
2018-11-15 01:46:56 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,10 @@
 def position_taken?(board, index)
-  if (board [index] == "X") || (board [index] == "O")
-    "true"
-  elsif board [index] == " "
+  if board [index] == " "
     "false"
   elsif board [index] == ""
     "false"
   else board [index] == nil
     "false"
+  else (board [index] == "X") || (board [index] == "O")
   end
 end
